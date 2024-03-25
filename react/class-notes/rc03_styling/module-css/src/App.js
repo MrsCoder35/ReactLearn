64b3259d9a151633c847eb5ddee129f96687ce42
cerentendar <cerentendar@gmail.com>
2024-03-25 19:10:29 +0300

    props

---
+++ react/class-notes/rc03_styling/module-css/src/App.js
@@ -0,0 +1,15 @@
+//? defoult exportun importu, istediğimiz isimle import edebiliriz
+
+import Cat from "./components/cat";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Cat/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ react/class-notes/rc03_styling/module-css/src/components/cat.jsx
@@ -0,0 +1,11 @@
+const Cat = () => {
+    return(
+        <div>
+        <img src="" alt="cat-img" />
+        <p>Color:</p>
+        <hr />
+        </div>
+    )
+}
+
+export default Cat
