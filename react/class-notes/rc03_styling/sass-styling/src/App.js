c10630c251f003f60b6d91992c9bb062b67ae508
cerentendar <cerentendar@gmail.com>
2024-03-28 00:10:39 +0300

    fns

--- react/class-notes/rc03_styling/sass-styling/src/App.js
+++ react/class-notes/rc03_styling/sass-styling/src/App.js
@@ -1,11 +1,17 @@
-
-
+import Header from "./components/Header";
+//? SCSS stylesheeti import ettik
+import "./sass/app.scss";
+import data from "./data";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      {data.map(({ id, name, job, img, comment }) => (
+        <Card key={id} name={name} job={job} img={img} comment={comment} />
+      ))}
+    </>
   );
 }
 
