d245a61f0cb56d630fd434c31febad76c54e8d8f
cerentendar <cerentendar@gmail.com>
2024-03-29 16:47:25 +0300

    v3

--- react/class-notes/rc04_products-list/src/App.js
+++ react/class-notes/rc04_products-list/src/App.js
@@ -1,10 +1,16 @@
+import Header from "./components/header/Header";
+import ProductList from "./components/products/ProductList";
+
+import { products, categories } from "./helper/data";
 
 
 
 function App() {
+  // console.log(products, categories);
   return (
     <div className="App">
-      
+      <Header categories = {categories} text="Products List"/>
+      <ProductList products={products}/>
     </div>
   );
 }
