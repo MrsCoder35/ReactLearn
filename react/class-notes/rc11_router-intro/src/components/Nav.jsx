9ee83db8c96de79b2e7fff1500a39fd6f90e87bb
cerentendar <cerentendar@gmail.com>
2024-04-14 21:00:34 +0300

    v3

--- react/class-notes/rc11_router-intro/src/components/Nav.jsx
+++ react/class-notes/rc11_router-intro/src/components/Nav.jsx
@@ -1,17 +1,21 @@
+import { Link, NavLink } from "react-router-dom"
+
 const Nav = () => {
   return (
-    <ul>
-      <li>
-        <a href="/">Home</a>
-      </li>
-      <li>
-        <a href="/instructors">Instructors</a>
-      </li>
-      <li>
-        <a href="/contact">Contact</a>
-      </li>
-    </ul>
-  );
-};
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/instructors">Instructors</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
 
-export default Nav;
+export default Nav
