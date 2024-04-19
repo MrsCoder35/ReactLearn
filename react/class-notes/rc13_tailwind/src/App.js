efcb9cf2416c1fffc21af50c41a5511c721631bf
cerentendar <cerentendar@gmail.com>
2024-04-19 20:41:10 +0300

    flex and responsive

--- react/class-notes/rc13_tailwind/src/App.js
+++ react/class-notes/rc13_tailwind/src/App.js
@@ -1,6 +1,8 @@
-// import Typografy from "./components/Typografy";
+import Typografy from "./components/Typografy";
 
 import SandS from "./components/SandS";
+import HoverAndFocus from "./components/HoverAndFocus";
+import FlexAndResponsive from "./components/FlexAndResponsive";
 
 
 
@@ -11,7 +13,9 @@ function App() {
     <div className="App">
       <h1 className="text-center text-2xl text-purple-700">Hello Tailwind</h1>
       {/* <Typografy/> */}
-      <SandS/>
+      {/* <SandS/> */}
+      {/* <HoverAndFocus/> */}
+      <FlexAndResponsive/>
       
      
       
