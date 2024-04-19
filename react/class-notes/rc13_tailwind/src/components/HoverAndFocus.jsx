403560cde9e5139da5dfe5e1c794199570d88e3b
cerentendar <cerentendar@gmail.com>
2024-04-19 20:19:36 +0300

    hover and focus

---
+++ react/class-notes/rc13_tailwind/src/components/HoverAndFocus.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HoverAndFocus = () => {
+  return (
+    <>
+    <div>
+        <button className="w-40 bg-sky-400 hover:bg-sky-600 hover:shadow-lg text-slate-50 rounded-full py-2 mx-1">Save</button>
+        <button className="text-white bg-yellow-400 px-5 py-2.5 rounded-lg hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 ml-2">Yellow</button>
+        </div>
+<div className="m-3">
+    <input type="email" className="border border-gray-300 text-gray-900 focus:outline-red-500 block w-1/2 p-3 rounded-lg peer" />
+</div>
+<p className="mt-2 text-red-600 text-sm invisible peer-invalid:visible">Please provide a valid email address</p>
+
+        </>
+ 
+
+    )
+
+}
+
+export default HoverAndFocus
