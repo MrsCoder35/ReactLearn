f477cdadfbc4ee78f157f08bc8bc631327c40712
cerentendar <cerentendar@gmail.com>
2024-04-19 13:03:01 +0300

    p

--- react/class-notes/rc13_tailwind/src/App.js
+++ react/class-notes/rc13_tailwind/src/App.js
@@ -1,9 +1,11 @@
+import Typografy from "./components/Typografy";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Tailwind</h1>
+      <h1 className="text-center text-2xl text-purple-700">Hello Tailwind</h1>
+      <Typografy/>
     </div>
   );
 }
---
+++ react/class-notes/rc13_tailwind/src/components/Typografy.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Typografy = () => {
+  return (
+    <div>
+        {/* Typografy */}
+        <h3 className="text-2xl">Typografy</h3>
+        <p className="text-sm">Typografy</p>
+        <p className="text-lg">Typografy</p>
+        <p className="text-xl">Typografy</p>
+        <p className="text-2xl">Typografy</p>
+        <h2 className="text-3xl">Typografy</h2>
+        <h1 className="text-4xl">Typografy</h1>
+        <h1 className="text-[60px]">Typografy</h1>
+        <h1 className="text-base/9">Typografy</h1>   
+        </div>
+  )
+}
+
+export default Typografy
