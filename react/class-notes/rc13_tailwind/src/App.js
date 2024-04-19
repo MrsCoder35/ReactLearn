0972b39fd2bc03588076f75715ed80a4e9132fab
cerentendar <cerentendar@gmail.com>
2024-04-19 14:05:02 +0300

    padding marjin Spacing and Sizing

--- react/class-notes/rc13_tailwind/src/App.js
+++ react/class-notes/rc13_tailwind/src/App.js
@@ -1,11 +1,21 @@
-import Typografy from "./components/Typografy";
+// import Typografy from "./components/Typografy";
+
+import SandS from "./components/SandS";
+
+
+
 
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-center text-2xl text-purple-700">Hello Tailwind</h1>
-      <Typografy/>
+      {/* <Typografy/> */}
+      <SandS/>
+      
+     
+      
+      
     </div>
   );
 }
---
+++ react/class-notes/rc13_tailwind/src/components/SandS.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SandS = () => {
+  return (
+    <div>
+        <h3>SpacingAndSizing</h3>
+        <button className="bg-blue-400 text-white p-4 m-8">Tıkla</button>
+        <button className="bg-blue-400 text-white px-4 ms-4">Tıkla</button>
+        <button className="bg-blue-400 text-white px-8 ml-4">Tıkla</button>
+        <button className="bg-blue-400 text-white px-4 py-2 mx-8">Tıkla</button>
+        </div>
+  )
+}
+
+export default SandS
