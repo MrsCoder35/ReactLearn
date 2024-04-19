0f62388363607033ea9aae202580382cc8f66b8e
cerentendar <cerentendar@gmail.com>
2024-04-19 13:44:09 +0300

    typografy done

--- react/class-notes/rc13_tailwind/src/components/Typografy.jsx
+++ react/class-notes/rc13_tailwind/src/components/Typografy.jsx
@@ -1,20 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const Typografy = () => {
   return (
     <div>
-        {/* Typografy */}
-        <h3 className="text-2xl">Typografy</h3>
-        <p className="text-sm">Typografy</p>
-        <p className="text-lg">Typografy</p>
-        <p className="text-xl">Typografy</p>
-        <p className="text-2xl">Typografy</p>
-        <h2 className="text-3xl">Typografy</h2>
-        <h1 className="text-4xl">Typografy</h1>
-        <h1 className="text-[60px]">Typografy</h1>
-        <h1 className="text-base/9">Typografy</h1>   
-        </div>
-  )
-}
+      {/* Typografy */}
+      <h3 className="text-2xl">Typografy</h3>
+      <p className="text-sm">Typografy</p>
+      <p className="text-lg">Typografy</p>
+      <p className="text-xl">Typografy</p>
+      <p className="text-2xl">Typografy</p>
+      <h2 className="text-3xl">Typografy</h2>
+      <h1 className="text-4xl">Typografy</h1>
+      <h1 className="text-[60px]">Typografy</h1>
+      {/* line-heigt */}
+      <h1 className="text-base/9">Typografy</h1>
+      {/* line clamp   */}
+      <article>
+        <p className="line-clamp-3">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi illo
+          quidem explicabo neque ipsam? Cupiditate, at eaque officiis repellat
+          asperiores laboriosam quae nobis veniam officia et iste cum mollitia
+          excepturi maiores adipisci quod est consequuntur possimus. Non vero,
+          perspiciatis tempore hic quas necessitatibus quae beatae adipisci quis
+          amet nam eum fugit dolorum veritatis unde cumque, odio iusto. Vitae
+          quam a tempora ipsam! Quos incidunt libero aut iusto, ipsum sint.
+          Pariatur, quaerat numquam autem unde corrupti corporis itaque iure
+          debitis porro!
+        </p>
+      </article>
 
-export default Typografy
+      <p className="text-center">Merhaba</p> 
+      <p className="underline">Merhaba</p>
+      <p className="line-through">Merhaba</p>
+      <p className="uppercase">Merhaba</p> 
+      <p className="lowercase">Merhaba</p>
+      <p className="text-red-500">Merhaba</p>
+      <p className="text-green-900">Merhaba</p>
+      <p className="text-[#4793AF] font-bold">Merhaba</p>
+      <p className="text-[rgb(251,109,72)]">Merhaba</p>
+      <p className="bg-slate-600">Merhaba</p>
+      <p className="bg-[#D74B76]">Merhaba</p>
+      <p className="">Merhaba</p>
+      <p className="">Merhaba</p>
+      <p className="">Merhaba</p>
+      <p className="">Merhaba</p>
+      <p className="">Merhaba</p>
+      <p className="">Merhaba</p>
+    </div>
+  );
+};
+
+export default Typografy;
