5cb18f72534f759d7fcc3c62f0e98239bdbc51c2
cerentendar <cerentendar@gmail.com>
2024-04-19 14:14:29 +0300

    rounded

--- react/class-notes/rc13_tailwind/src/components/SandS.jsx
+++ react/class-notes/rc13_tailwind/src/components/SandS.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const SandS = () => {
   return (
-    <div>
+    <>
+      <div>
         <h3>SpacingAndSizing</h3>
         <button className="bg-blue-400 text-white p-4 m-8">Tıkla</button>
         <button className="bg-blue-400 text-white px-4 ms-4">Tıkla</button>
         <button className="bg-blue-400 text-white px-8 ml-4">Tıkla</button>
         <button className="bg-blue-400 text-white px-4 py-2 mx-8">Tıkla</button>
-        </div>
-  )
-}
+      </div>
+      <div>
+        <button className="bg-green-600 text-white mx-4 px-4 py-2 border border-yellow-600 rounded-md">
+            
+          Tıkla
+        </button>
+        <button className="bg-green-600 text-white mx-4 px-4 py-2 border-[3px] border-yellow-600 rounded-[10px]">
+            
+          Tıkla
+        </button>
+        <button className="bg-green-600 text-white mx-4 px-6 py-6 w-20 h-20 rounded-full">
+            
+          Tıkla
+        </button>
+        <button className="bg-green-600 text-white mx-4 px-6 py-6 w-20 h-20 rounded-[50%]">
+            
+            Tıkla
+          </button>
+      </div>
+    </>
+  );
+};
 
-export default SandS
+export default SandS;
