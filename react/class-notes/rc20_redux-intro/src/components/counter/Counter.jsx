dc3ffa38677894e5abd1c6c6b018192d753d49e6
cerentendar <cerentendar@gmail.com>
2024-04-30 14:25:45 +0300

    inc dec reset

--- react/class-notes/rc20_redux-intro/src/components/counter/Counter.jsx
+++ react/class-notes/rc20_redux-intro/src/components/counter/Counter.jsx
@@ -12,8 +12,8 @@ const Counter = () => {
       <h1>counter:{count}</h1>
       <div>
         <button className="counter-button positive" onClick= {() => dispatch({type: "ARTTIR"})}>increase</button>
-        <button className="counter-button zero">reset</button>
-        <button className="counter-button negative">decrease</button>
+        <button className="counter-button zero" onClick= {() => dispatch({type : "SIL"})}>reset</button>
+        <button className="counter-button negative" onClick={() => dispatch({type: "AZALT"})}>decrease</button>
       </div>
     </div>
   )
