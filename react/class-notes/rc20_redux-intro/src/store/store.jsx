f5875753e50e2f87f0354bbb8da73b1d2a9fc416
cerentendar <cerentendar@gmail.com>
2024-04-30 11:13:09 +0300

    store oluşturma

---
+++ react/class-notes/rc20_redux-intro/src/store/store.jsx
@@ -0,0 +1,6 @@
+import {legacy_createStore as createStore} from "redux"
+import { counterReducer } from "./counterReducer"
+
+//? Store'un oluşturulması
+
+export const store = createStore(counterReducer)
