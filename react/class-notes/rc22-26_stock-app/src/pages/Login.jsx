1c7164777deb118d17505326a015b1160123668f
cerentendar <cerentendar@gmail.com>
2024-05-15 20:34:28 +0300

    login

--- react/class-notes/rc22-26_stock-app/src/pages/Login.jsx
+++ react/class-notes/rc22-26_stock-app/src/pages/Login.jsx
@@ -4,12 +4,21 @@ import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
 import LockIcon from "@mui/icons-material/Lock"
 import image from "../assets/result.svg"
-import { Link } from "react-router-dom"
+import { Form, Link } from "react-router-dom"
 import Box from "@mui/material/Box"
 import TextField from "@mui/material/TextField"
 import { Button } from "@mui/material"
+import {Formik} from "formik"
+import { object, string} from 'yup';
+
+
 
 const Login = () => {
+  const loginSchema = object({
+    
+    email: string().email().required(),
+   password: string().required(),
+  });
     return (
     <Container maxWidth="lg">
       <Grid
@@ -46,29 +55,51 @@ const Login = () => {
           >
             Login
           </Typography>
-
-          <Box
-            component="form"
-            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-          >
-            <TextField
-              label="Email"
-              name="email"
-              id="email"
-              type="email"
-              variant="outlined"
-            />
-            <TextField
-              label="password"
-              name="password"
-              id="password"
-              type="password"
-              variant="outlined"
-            />
-            <Button variant="contained" type="submit">
-              Submit
-            </Button>
-          </Box>
+<Formik 
+initialValues = {{email: "", password: ""}} 
+validationSchema={loginSchema}
+onSubmit = {(values, actions) => {
+  //TODO
+  //? PST (login)
+  //? Toastify
+  //? Global state güncellenmesi
+  //? form resetleme
+  //? navigate
+}}>
+  {({values, handleChange, handleBlur}) => (
+    <Form> <Box
+            
+    sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+  >
+    <TextField
+      label="Email"
+      name="email"
+      id="email"
+      type="email"
+      variant="outlined"
+      value = {values.email}
+      onChange = {handleChange}
+      onBlur = {handleBlur}
+      error = {true}
+      helperText = {"Deneme"}
+    />
+    <TextField
+      label="password"
+      name="password"
+      id="password"
+      type="password"
+      variant="outlined"
+      value = {values.password}
+      onChange = {handleChange}
+      onBlur = {handleBlur}
+    />
+    <Button variant="contained" type="submit">
+      Submit
+    </Button>
+  </Box></Form>
+  )}
+</Formik>
+         
 
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/register">Do you have not an account?</Link>
